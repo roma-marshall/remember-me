8d2c65a98221e7ceeb955952c4c8c7d8add3ed55
roma_marshall <roma_marshall@outlook.com>
2022-10-31 20:01:40 +0100

    add stored resources

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      storedResources: {
+        id: 'official-guide',
+        title: 'Official Guide',
+        description: 'The official Vue 3 documentation',
+        link: 'https://vuejs.org'
+      }
+    }
   }
 }
 </script>
