8f7b68db95b98c779429150e31e833f626291ff3
roma_marshall <roma_marshall@outlook.com>
2022-11-08 20:13:56 +0100

    v-for edit

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -1,10 +1,19 @@
 <template>
-
+  <ul>
+    <learning-resource
+        v-for="res in resources"
+        :key="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+    >
+    </learning-resource>
+  </ul>
 </template>
 
 <script>
 export default {
-  
+  props: ['resources']
 }
 </script>
 
