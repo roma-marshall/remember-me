40aa6b223b352933dad24e39d01cf22cb2387e4b
roma_marshall <roma_marshall@outlook.com>
2022-11-12 14:32:48 +0100

    UI component styled

--- src/components/UI/baseCard.vue
+++ src/components/UI/baseCard.vue
@@ -9,7 +9,7 @@
 <style scoped>
 div {
   border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
   padding: 1rem;
   margin: 2rem auto;
   max-width: 40rem;
