f07985ed3da0bda2dbf1ac0392ae2287c2b9c2e8
roma_marshall <roma_marshall@outlook.com>
2022-11-22 07:47:51 +0100

    change button -> base-button

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button>Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
