a652f5e88f40902c80630e6634b0798cf12ef671
roma_marshall <roma_marshall@outlook.com>
2022-12-15 17:36:42 +0100

    fix & code refactoring

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -17,7 +17,21 @@ export default {
   },
   data() {
     return {
-      selectedTab: 'stored-resources';
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue 3 documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google Inc.',
+          description: 'Learn to google',
+          link: 'https://google.com'
+        }
+      ]
     };
   },
   methods: {
