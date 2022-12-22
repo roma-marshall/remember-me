05e6df935d32a848bac87c7a15ed902dd95304ff
roma_marshall <roma_marshall@outlook.com>
2022-12-23 00:03:53 +0100

    add computed

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,7 +1,7 @@
 <template>
   <base-card>
-    <base-button @click="selectedTab('stored-resources')" :mode="selectedTab === 'stored-resources' ? null : 'flat'">Stored Resources</base-button>
-    <base-button @click="selectedTab('add-resource')">Add Resource</base-button>
+    <base-button @click="selectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="selectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
   <component :is="selectedTab"></component>
 </template>
@@ -37,6 +37,14 @@ export default {
   provide() {
     return {
       resources: this.storedResources
+    }
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
     }
   },
   methods: {
