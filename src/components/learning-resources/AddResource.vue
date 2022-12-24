aca26647b5e93a873c8644205a0dfacfc80373a6
roma_marshall <roma_marshall@outlook.com>
2022-12-24 11:39:32 +0100

    add textarea

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -5,6 +5,10 @@
         <label for="title">Title</label>
         <input id="title" name="title" type="text">
       </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3"></textarea>
+      </div>
     </form>
   </base-card>
 </template>
