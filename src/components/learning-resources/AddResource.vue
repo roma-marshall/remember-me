3600c59588ece4c27c1b2f9144c432bec59465ed
roma_marshall <roma_marshall@outlook.com>
2022-12-27 20:47:22 +0100

    add some styling

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -14,8 +14,36 @@
         <input id="link" name="link" type="url">
       </div>
       <div>
-        <base-button type="submit">Add Resource</base-button>
+        <base-button type="submit">Add Resource </base-button>
       </div>
     </form>
   </base-card>
 </template>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
