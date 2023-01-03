d0aead1f50c3e552f8f07f4b11239871f4920c26
roma_marshall <roma_marshall@outlook.com>
2023-01-03 19:10:24 +0100

    add ref

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -3,15 +3,15 @@
     <form>
       <div class="form-control">
         <label for="title">Title</label>
-        <input id="title" name="title" type="text" ref="">
+        <input id="title" name="title" type="text" ref="titleInput">
       </div>
       <div class="form-control">
         <label for="description">Description</label>
-        <textarea id="description" name="description" rows="3"></textarea>
+        <textarea id="description" name="description" rows="3" ref="descriptionInput"></textarea>
       </div>
       <div class="form-control">
         <label for="link">Link</label>
-        <input id="link" name="link" type="url">
+        <input id="link" name="link" type="url" ref="linkInput">
       </div>
       <div>
         <base-button type="submit">Add Resource </base-button>
